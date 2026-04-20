145c9698f616174177fe812976d6006139e81348
priyanshumistry73 <priyanshumistry000@gmail.com>
2026-04-21 00:24:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const URL = "/public/";
+
+let model;
+
+async function init() {
+    model = await tmImage.load(URL + "model.json", URL + "metadata.json");
+}
+
+init();
+
+document.getElementById("upload").addEventListener("change", async (e) => {
+    const file = e.target.files[0];
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = async () => {
+        const prediction = await model.predict(img);
+        let result = "";
+
+        prediction.forEach(p => {
+            result += `${p.className}: ${p.probability.toFixed(2)}\n`;
+        });
+
+        document.getElementById("result").innerText = result;
+    };
+});
